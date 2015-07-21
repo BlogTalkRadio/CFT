4adfb37d14256fa268c05d8cb070f7c42fb13d3f
pmmax <pmmax.net@gmail.com>
2015-07-21 13:16:33 -0300

    Added exception logging

--- CFT/Program.cs
+++ CFT/Program.cs
@@ -10,12 +10,29 @@ namespace BlogTalkRadio.Tools.CFT
         private static void Main(string[] args)
         {
             // log to console by default
-            Trace.AutoFlush = true;
-            var traceListener = new ConsoleTraceListener();
-            traceListener.TraceOutputOptions = TraceOptions.None;
-            Trace.Listeners.Add(traceListener);
+            try
+            {
+                Trace.AutoFlush = true;
+                var traceListener = new ConsoleTraceListener();
+                traceListener.TraceOutputOptions = TraceOptions.None;
+                Trace.Listeners.Add(traceListener);
+                Parser.RunConsole<App>(args);
+            }
+            catch (Exception ex)
+            {
+                WriteException(ex);
+            }
+        }
 
-            Parser.RunConsole<App>(args);
+        public static void WriteException(Exception ex)
+        {
+            while (ex != null)
+            {
+                Console.WriteLine(ex.GetType().FullName);
+                Console.WriteLine(ex.Message);
+                Console.WriteLine(ex.StackTrace);
+                ex = ex.InnerException;
+            }
         }
     }
 }
